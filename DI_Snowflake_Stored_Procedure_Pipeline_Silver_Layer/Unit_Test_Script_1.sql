c4b22a00222045499c6bb1e0e11fe6f06b7bd825
karthik061299 <karthikeyancoc07@gmail.com>
2025-08-25 19:16:22 +0530

    Add or update file: Unit_Test_Script_1.sql

---
+++ DI_Snowflake_Stored_Procedure_Pipeline_Silver_Layer/Unit_Test_Script_1.sql
@@ -0,0 +1,4 @@
+_____________________________________________
+## *Author*: AAVA
+## *Created on*:   
+## *Description*: Comprehensive unit test suite for sp_bronze_to_silver_etl stored procedure that validates data transformations
