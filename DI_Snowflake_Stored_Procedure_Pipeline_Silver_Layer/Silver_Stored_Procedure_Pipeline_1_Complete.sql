240943861e1cbedb4670d9e8ec6715c7e997fa84
karthik061299 <karthikeyancoc07@gmail.com>
2025-08-25 13:20:20 +0530

    Add or update file: Silver_Stored_Procedure_Pipeline_1_Complete.sql

---
+++ DI_Snowflake_Stored_Procedure_Pipeline_Silver_Layer/Silver_Stored_Procedure_Pipeline_1_Complete.sql
@@ -0,0 +1,13 @@
+_____________________________________________
+## *Author*: AAVA
+## *Created on*:   
+## *Description*: Comprehensive Snowflake stored procedure for Bronze to Silver layer ETL processing in Inventory Management System
+## *Version*: 1 
+## *Updated on*: 
+_____________________________________________
+
+-- =====================================================
+-- BRONZE TO SILVER LAYER ETL STORED PROCEDURE
+-- =====================================================
+-- Purpose: Process data from Bronze layer to Silver layer with comprehensive
+--          data validation
