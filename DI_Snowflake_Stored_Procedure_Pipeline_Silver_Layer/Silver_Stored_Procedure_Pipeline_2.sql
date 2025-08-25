4ca1cc7916ad7f9d5d378e9c69f68ce69859f726
karthik061299 <karthikeyancoc07@gmail.com>
2025-08-25 13:26:43 +0530

    Add or update file: Silver_Stored_Procedure_Pipeline_2.sql

---
+++ DI_Snowflake_Stored_Procedure_Pipeline_Silver_Layer/Silver_Stored_Procedure_Pipeline_2.sql
@@ -0,0 +1,6 @@
+_____________________________________________
+## *Author*: AAVA
+## *Created on*:   
+## *Description*: Complete Snowflake stored procedure for Bronze to Silver layer ETL processing covering all 10 tables in Inventory Management System
+## *Version*: 2 
+## *Changes*: Added processing for all remaining 8 tables (si_suppliers
